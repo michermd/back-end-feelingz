77b2f058719d50e37a6686aaf91f3034c9985c84
michermd <michermd@gmail.com>
2018-09-21 12:59:23 -0400

    Created Models and Relationships for User and MoodPosts

--- feelingz_app/db/schema.rb
+++ feelingz_app/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_152553) do
+ActiveRecord::Schema.define(version: 2018_09_21_154803) do
 
   create_table "mood_posts", force: :cascade do |t|
     t.string "img_url"
     t.string "emotion"
     t.string "mood"
     t.string "journal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
