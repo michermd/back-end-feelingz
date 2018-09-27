fd4a924b4fadfe60286e741c5ba779ed3dc1e8e7
michermd <michermd@gmail.com>
2018-09-27 14:50:55 -0400

    validation for signup

--- feelingz/app/models/mood_post.rb
+++ feelingz/app/models/mood_post.rb
@@ -1,3 +1,4 @@
 class MoodPost < ApplicationRecord
+  validates :img_url, :emotion, :mood, :journal, presence: true
   belongs_to :user
 end
--- feelingz/app/models/user.rb
+++ feelingz/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :user_name, uniqueness: true
+  validates :first_name, :last_name, :user_name, :password, presence: true
   has_many :mood_post
 end
